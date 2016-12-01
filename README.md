2ecb83a23dc656a702aa2a5c6b436d006e8a292f
bearyang <bearyang@pptv.com>
2016-12-01 10:47:02 +0800

    add table layout learn

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 #这是一个日常练习的程序
+
+##TableLayout
+Tablelayout类以行和列的形式对控件进行管理，每一行为一个TableRow对象，或一个View控件。
+
+当为TableRow对象时，可在TableRow下添加子控件，默认情况下，每个子控件占据一列。
+
+当为View时，该View将独占一行。
+
+
+TableRow中的控件不能指定宽度，宽度永远是match_parent。
+
+列的宽度由该列所有行的一个单元格决定。
+
+shrinkColumns="0,1" 表示第0列和第1列可以收缩
+
+stretchColumns="0,1" 表示第0列和第1列可以拉伸
+
+collapseColumns="0,1" 表示隐藏第0列和第1列，相当于gone，不占空间
+
+layout_span="2" 表示该控件占据两列的空间
+
+words：
+
+row 行
+
+column 列
+
+shrink 收缩
+
+stretch 拉伸
--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.hello.activity.TableLayoutActivity;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,9 @@ public class MainActivity extends Activity {
     }
 
     private void initView() {
+        Button btnTableLayout = (Button) findViewById(R.id.btn_table_layout);
+
+        btnTableLayout.setOnClickListener(btnClickListener);
 
     }
 
@@ -28,6 +34,11 @@ public class MainActivity extends Activity {
     View.OnClickListener btnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_table_layout:
+                    startActivity(TableLayoutActivity.class);
+                    break;
+            }
         }
     };
 
