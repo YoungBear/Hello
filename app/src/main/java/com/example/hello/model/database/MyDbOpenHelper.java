4ec7b4f62792f4baea051cfaadb46a2710f7029b
bearyang <bearyang@pptv.com>
2017-03-01 18:34:06 +0800

    add provider test

---
+++ app/src/main/java/com/example/hello/model/database/MyDbOpenHelper.java
@@ -0,0 +1,44 @@
+package com.example.hello.model.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created on 2017/3/1.
+ */
+
+public class MyDbOpenHelper extends SQLiteOpenHelper {
+
+    public static final String TAG = MyDbOpenHelper.class.getSimpleName();
+
+    private static final String DB_NAME = "book_provider.db";
+    public static final String BOOK_TABLE_NAME = "book";
+    public static final String USER_TABLE_NAME = "user";
+
+    private static final int DB_VERSION = 1;
+
+    private static final String CREATE_BOOK_TABLE = "CREATE TABLE IF NOT EXISTS "
+            + BOOK_TABLE_NAME + "(_id INTEGER PRIMARY KEY," + "name TEXT)";
+
+    public static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS "
+            + USER_TABLE_NAME + "(_id INTEGER PRIMARY KEY," + "name TEXT," + "sex INT," + "age INTEGER)";
+
+
+    public MyDbOpenHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK_TABLE);
+        db.execSQL(CREATE_USER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(TAG, "onUpgrade, oldVersion: " + oldVersion + ", newVersion: " + newVersion);
+
+    }
+}
