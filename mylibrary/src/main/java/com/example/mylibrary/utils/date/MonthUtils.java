1fa5ad65f9ef7949be56b373f97148cb4137aeb4
yangshaoxiong <yangshaoxiong@boco.com.cn>
2017-09-23 16:10:07 +0800

    add MonthUtils

---
+++ mylibrary/src/main/java/com/example/mylibrary/utils/date/MonthUtils.java
@@ -0,0 +1,55 @@
+package com.example.mylibrary.utils.date;
+
+import java.util.Calendar;
+
+/**
+ * Created by ysx on 2017/9/23.
+ */
+
+public final class MonthUtils {
+    private static final String TAG = "MonthUtils";
+
+    private MonthUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * @param i 0表示当前月份，1表示下一个月份，-1表示上一个月份
+     * @return 返回月份的int值 1~12
+     */
+    public static int getMonth(int i) {
+        Calendar calendar = Calendar.getInstance();
+        //获取一年中的第几个月，从0~11
+        int index = calendar.get(Calendar.MONTH);
+        int result = index + 1 + i;
+        return (result + 12) % 12;
+    }
+
+    /**
+     * @return 当前月份，从1~12
+     */
+    public static int getCurrentMonth() {
+        return getMonth(0);
+    }
+
+    /**
+     * @return 返回上个月的月份 1~12
+     */
+    public static int getLastMonth() {
+        return getMonth(-1);
+    }
+
+    /**
+     * @return 返回上两个月的月份 1~12
+     */
+    public static int getLastTwoMonth() {
+        return getMonth(-2);
+    }
+
+    /**
+     * @return 返回上三个月的月份 1~12
+     */
+    public static int getLastThreeMonth() {
+        return getMonth(-3);
+    }
+}
